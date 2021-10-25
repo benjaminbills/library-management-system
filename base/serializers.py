1d4b16f422ed93d9d920932de07002a581738d4d
Benjamin Obafemi <benjaminoba234@gmail.com>
2021-10-25 14:57:57 +0300

    crud for user

---
+++ base/serializers.py
@@ -0,0 +1,30 @@
+from django.db import models
+from rest_framework import serializers
+from base.models import NewUser
+from rest_framework_simplejwt.tokens import RefreshToken
+
+class UserSerializer(serializers.ModelSerializer):
+  user_name=serializers.SerializerMethodField(read_only=True)
+  id=serializers.SerializerMethodField(read_only=True)
+  isAdmin=serializers.SerializerMethodField(read_only=True)
+  class Meta:
+    model = NewUser
+    fields = ['id', 'user_name', 'email','isAdmin']
+  def get_id(self, obj):
+    return obj.id  
+  def get_isAdmin(self, obj):
+    return obj.is_staff
+  def get_user_name(self, obj):
+    user_name = obj.user_name
+    if user_name== '':
+      user_name = obj.email
+    return user_name
+
+class UserSerializerWithToken(UserSerializer):
+  token = serializers.SerializerMethodField(read_only=True)
+  class Meta:
+    model = NewUser
+    fields = ['id', 'user_name', 'email','isAdmin', 'token' ]
+  def get_token(self,obj):
+    token = RefreshToken.for_user(obj)
+    return str(token.access_token)
---
+++ base/views/user_views.py
@@ -0,0 +1,61 @@
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from django.contrib.auth.hashers import make_password
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from base.serializers import UserSerializer, UserSerializerWithToken
+
+
+from base.models import NewUser
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+  def validate(self, attrs):
+      data = super().validate(attrs)
+
+      serializer = UserSerializerWithToken(self.user).data
+
+      for k, v in serializer.items():
+        data[k] = v
+
+      return data
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
+@api_view(['POST'])
+def registerUser(request):
+  data=request.data
+  try:
+    user = NewUser.objects.create(
+      user_name = data['user_name'],
+      email = data['email'],
+      password = make_password(data['password'])
+    )
+    serializer = UserSerializerWithToken(user, many=False)
+    
+    return Response(serializer.data)
+  except:
+    message = {'detail':'User with this email or username already exist'}
+    return Response(message, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getUserByID(request, pk):
+  user = NewUser.objects.get(id=pk)
+  serializer = UserSerializer(user, many=False)
+  return Response(serializer.data)
+
+@api_view(['DELETE'])
+@permission_classes([IsAdminUser])
+def deleteUser(request, pk):
+  try:
+    user = NewUser.objects.get(id=pk)
+    user.delete()
+    return Response('User was deleted successfully')
+  except:
+    message = {'detail':'User does not exist'}
+    return Response(message, status=status.HTTP_400_BAD_REQUEST )
