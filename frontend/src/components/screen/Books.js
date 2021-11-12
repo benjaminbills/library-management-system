3171bdcef1e3337ee08d4ac255891ee3d7f4a299
benjaminbills <benjaminoba234@gmail.com>
2021-11-12 11:00:41 +0300

    fixing error

--- frontend/src/components/screen/Books.js
+++ frontend/src/components/screen/Books.js
@@ -40,7 +40,7 @@ function Books(props) {
     if (window.confirm("Are you sure you want to delete this product")) {
       dispatch(deleteBook(id));
     }
-    window.location.reload();
+    window.location.reloader();
   };
   const assignBookHandler = (id) => {
     dispatch(assignBook(props.studentId, id));
